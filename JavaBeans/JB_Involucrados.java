73a33dc2123d545fb915060070d8533c21de85b5
Darkexx <fertojonathan@gmail.com>
2023-05-15 20:17:01 -0600

    Cree todos los Javabeans

---
+++ JavaBeans/JB_Involucrados.java
@@ -0,0 +1,68 @@
+package JavaBeans;
+import java.io.Serializable;
+
+public class JB_Involucrados implements Serializable{
+	private String id_at;
+	private int id_h;
+	private int id_agent;
+	private String fecha;
+
+	public JB_Involucrados() {
+		
+	}
+	
+	public JB_Involucrados(
+			
+			int id_at,
+			int id_h,
+			int id_agent,
+			String fecha
+			
+			) {
+			
+		this.id_at=id_at;
+		this.id_h=id_h;
+		this.id_agent=id_agent;
+		this.fecha=fecha;
+			
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// Getter y Setter ID_At
+	public String getIdAt() {
+	    return id_at;
+	}
+	
+	public void setIdAt(String idAt) {
+	    this.id_at = idAt;
+	}
+
+	// Getter y Setter ID_H
+	public int getIdH() {
+	    return id_h;
+	}
+
+	public void setIdH(int idH) {
+	    this.id_h = idH;
+	}
+
+	// Getter y Setter ID_Agent
+	public int getIdAgent() {
+	    return id_agent;
+	}
+
+	public void setIdAgent(int idAgent) {
+	    this.id_agent = idAgent;
+	}
+
+	// Getter y Setter Fecha
+	public String getFecha() {
+	    return fecha;
+	}
+
+	public void setFecha(String fecha) {
+	    this.fecha = fecha;
+	}
+
+}
---
+++ JavaBeans/JB_STARK.java
@@ -0,0 +1,46 @@
+package JavaBeans;
+import java.io.Serializable;
+
+public class JB_STARK implements Serializable{
+	private String id_ceo;
+	private String nombre;
+
+	public JB_STARK() {
+		
+	}
+	
+	public JB_STARK(
+			
+			int id_ceo,
+			String nombre
+			
+			) {
+			
+		this.id_ceo=id_ceo;
+		this.nombre=nombre;
+			
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// Getter y Setter ID_CEO
+	public String getIdCeo() {
+	    return id_ceo;
+	}
+	
+	public void setIdCeo(String idCeo) {
+	    this.id_ceo = idCeo;
+	}
+
+	// Getter y Setter Nombre
+	public String getNombre() {
+	    return nombre;
+	}
+
+	public void setNombre(String nombre) {
+	    this.nombre = nombre;
+	}
+
+	
+	
+}
