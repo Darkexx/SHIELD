83c84b9df56ff1f2d082d29eb191264be0e382bb
Darkexx <fertojonathan@gmail.com>
2023-05-22 21:57:23 -0600

    El archivo de prueba es funcional (main.java)

--- src/main.java
+++ src/main.java
@@ -1,4 +1,21 @@
 
+import java.sql.*;
+import java.util.*;
+
+import Datos.Conexion;
+import Datos.DAO_Agentes;
+import JavaBeans.JB_Agentes;
+
 public class main {
+	
+	public static void main(String[] args) {
+		Conexion conn = new Conexion();
+		Connection connection = null;
+		
+		JB_Agentes ag = new JB_Agentes();
+		DAO_Agentes agdao = new DAO_Agentes();
+		
+		System.out.println(agdao.seleccionar());
+	}
 
 }
