48d682b82a0619dc10df860e7eda98fdea9d03de
Darkexx <fertojonathan@gmail.com>
2023-06-02 01:02:16 -0600

    Creacion del Servlet para el login

---
+++ src/main/java/Servlet/Servlet_Login.java
@@ -0,0 +1,70 @@
+package Servlet;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.*;
+
+import JavaBeans.JB_Users;
+import Datos.DAO_Users;
+import Datos.DAO_Lider;
+
+
+@WebServlet("/Servlet_Login")
+
+public class Servlet_Login extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+          DAO_Users usurdao = new DAO_Users();
+          String user = request.getParameter("EntUsu");
+          String password = request.getParameter("EntPass").trim();
+
+          JB_Users usur = usurdao.busca(user);
+
+        if (usur!=null) {
+            if (usur.getPass().equals(password)) {
+
+                 //request.setAttribute("usuario",usuario);
+
+                if (usur.getRoll().equals("Lider_Shield")) {
+                    //Si el rol es de admin reedirige al jsp correspondiente
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("indexLidShield.jsp");
+                    dispatcher.forward(request, response);
+                }
+                else if (usur.getRoll().equals("Lider_Heroes")) {
+                    //Si se trata de un empleado obtenemos su nombre  para saludarlo en el jsp
+                	
+                	int idint = Integer.parseInt(usur.getId_us());
+                	
+                    DAO_Lider lidao = new DAO_Lider();
+                    String nomlid = lidao.retnom(idint);
+
+                    request.setAttribute("nomlider",nomlid);
+
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("indexLidHer.jsp");
+                    dispatcher.forward(request, response);
+                }
+                else {
+                	
+                	System.out.println("no existe roll");
+                } 
+
+            }
+            else {
+            	System.out.println("Contraseña incorrecta");
+            }
+
+        }
+        else {
+        	System.out.println("no existe usuario");
+        }
+
+
+
+    }
+
+}
