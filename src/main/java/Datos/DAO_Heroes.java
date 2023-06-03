87581c95329eccc4680dcbbbd58b5526ada8da7a
Darkexx <fertojonathan@gmail.com>
2023-06-03 17:11:16 -0600

    Se agrego la consulta para que aparezca el nombre de los heroes y de las subdivisiones

--- src/main/java/Datos/DAO_Heroes.java
+++ src/main/java/Datos/DAO_Heroes.java
@@ -10,6 +10,7 @@ public class DAO_Heroes {
 	public static final String insertSQL = "INSERT INTO heroes(id_h,nombre,id_l,id_sub) VALUES (?,?,?,?)";
 	public static final String updateSQL = "UPDATE heroes SET nombre = ?, id_l = ?, id_sub = ? WHERE id_h = ?";
 	public static final String deleteSQL = "DELETE FROM heroes WHERE id_h = ?";
+	public static final String consulHer = "select heroes.id_h,heroes.nombre,lider.nombre as Lider,subdivision.nombre as nombre_sub from heroes join lider on heroes.id_l=lider.id_l join subdivision on heroes.id_sub=subdivision.id_sub";
 	
 	public List<JB_Heroes> seleccionar(){
 		Connection conn = null;
@@ -55,6 +56,57 @@ public class DAO_Heroes {
 			return heroes;
 		
 	}
+	
+	///////////////////-Consulta heroes ids con nombre-//////////////////////////////////////////
+	
+	public List<JB_Heroes> consulher(){
+		Connection conn = null;
+		Statement state = null;
+		ResultSet result = null;
+		JB_Heroes Ah = null;
+		
+			List<JB_Heroes> heroes = new ArrayList<>();
+			
+			try {
+				
+				conn = Conexion.getConnection();
+				state = conn.createStatement();
+				result = state.executeQuery(consulHer);
+				
+					while(result.next()) {
+						
+						//Aqui me quede
+						
+						int id_h = result.getInt("id_h");
+						String nombre = result.getString("nombre");
+						String id_lider = result.getString("Lider");
+						String id_subdiv = result.getString("nombre_sub");
+						//int id_l = result.getInt("id_l");
+						//String id_sub = result.getString("id_sub");
+						
+						Ah = new JB_Heroes(id_h,nombre,id_lider,id_subdiv);
+						heroes.add(Ah);
+						
+					}
+					
+					Conexion.close(result);
+					Conexion.close(state);
+					Conexion.close(conn);
+					
+					for(JB_Heroes c: heroes) {
+						System.out.println("id_h: " + c.getIdH());
+						System.out.println("Nombre: " + c.getNombre());
+						System.out.println("id_lider: " + c.getid_lider());
+						System.out.println("id_lider: " + c.getid_subdiv());
+					}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return heroes;
+	}
+	///////////////////////////////////////////////////////////////////////
 	
 	public int agregar(JB_Heroes Heroe) {
 		
