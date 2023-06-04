4a5bec496abc1a4ae32011caf2ebd587557810bb
Darkexx <fertojonathan@gmail.com>
2023-06-03 19:26:38 -0600

    Se agregaron datos a constructores para agregar un parametro a integrantes

--- src/main/java/JavaBeans/JB_Integrantes.java
+++ src/main/java/JavaBeans/JB_Integrantes.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 public class JB_Integrantes implements Serializable{
 	private int id_h;
 	private String nombre;
+	private int id_int;
 	//--Variables para consultas--//
 	private String nombre2;//Variable utilizada para consulta Heroe-Integrante
 
@@ -26,24 +27,27 @@ public class JB_Integrantes implements Serializable{
 	public JB_Integrantes(
 			
 			int id_h,
-			String nombre
+			String nombre,
+			int id_int
 			
 			) {
 			
 		this.id_h=id_h;
 		this.nombre=nombre;
+		this.id_int=id_int;
 			
 	}
 	
 public JB_Integrantes(
 			
 			String nombre,
-			String nombre2
-			
+			String nombre2,
+			int id_int
 			) {
 			
 		this.nombre=nombre;
 		this.nombre2=nombre2;
+		this.id_int=id_int;
 			
 	}
 
@@ -69,6 +73,17 @@ public JB_Integrantes(
 	public void setNombre(String nombre) {
 	    this.nombre = nombre;
 	}
+	
+	//
+	
+	public int getIdInt() {
+	    return id_int;
+	}
+
+	public void setIdInt(int id_int) {
+	    this.id_int = id_int;
+	}
+	
 	
 	//Getter y Setter Nombre2 (utilizada solo para consulta Heores-Integrantes)
 	public String getNombre2() {
