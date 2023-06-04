07b9268429aa6b18259a67a071085a50c4fe018a
Darkexx <fertojonathan@gmail.com>
2023-06-03 21:17:19 -0600

    Ya funciona el delete

--- src/main/java/Servlet/ServletHeroesDelete.java
+++ src/main/java/Servlet/ServletHeroesDelete.java
@@ -1,17 +1,9 @@
 package Servlet;
 
-import javax.annotation.Resource;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.*;
 import javax.servlet.*;
-import javax.sql.DataSource;
-import java.sql.*;
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.logging.*;
 
 import JavaBeans.JB_Heroes;
 import Datos.DAO_Heroes;
