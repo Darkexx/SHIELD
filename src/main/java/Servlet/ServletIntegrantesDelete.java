e35e86d48d37c164bdbd015570c07636ec375183
Darkexx <fertojonathan@gmail.com>
2023-06-03 21:20:29 -0600

    Ya funciona el Delte en integrantes

--- src/main/java/Servlet/ServletIntegrantesDelete.java
+++ src/main/java/Servlet/ServletIntegrantesDelete.java
@@ -1,9 +1,9 @@
 package Servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,12 +12,14 @@ import JavaBeans.JB_Integrantes;
 
 @WebServlet("/ServletIntegrantesDelete")
 
-public class ServletIntegrantesDelete {
+public class ServletIntegrantesDelete extends HttpServlet{
 	
+	private static final long serialVersionUID = 3142298690820546658L;
+
 	protected void doGet(HttpServletRequest rq, HttpServletResponse rp) throws IOException, ServletException{
 	
-		int id_h = Integer.parseInt(rq.getParameter(("id_h")));
-		JB_Integrantes inte = new JB_Integrantes(id_h);
+		int id_i = Integer.parseInt(rq.getParameter(("id_i")));
+		JB_Integrantes inte = new JB_Integrantes(id_i);
 		DAO_Integrantes intedao = new DAO_Integrantes();
 		intedao.borrar(inte);
 		rp.sendRedirect("");
