06c69ce1ca5b8e4b246c2413c9cd8fe6a32883eb
Darkexx <fertojonathan@gmail.com>
2023-06-07 00:44:15 -0600

    Creacion de una consulta mas para buscar el tipo de ataque en especifico

--- src/main/java/Datos/DAO_TipoAt.java
+++ src/main/java/Datos/DAO_TipoAt.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class DAO_TipoAt {
 	public static final String selectSQL = "SELECT * FROM tip_ataque";
+	public static final String selectConsu = "SELECT * FROM tip_ataque Where nom_tipo = ?";
 	public static final String insertSQL = "INSERT INTO tip_ataque(id_at,nom_tipo,reg_afect,mutant_afect,grupo_at,alien,bienes) VALUES (?,?,?,?,?,?,?)";
 	public static final String updateSQL = "UPDATE tip_ataque SET nom_tipo = ?, reg_afect = ?, grupo_at = ?, alien = ?, bienes = ? WHERE id_at = ?";
 	public static final String deleteSQL = "DELETE FROM tip_ataque WHERE id_at=?";
@@ -61,6 +62,65 @@ public class DAO_TipoAt {
 			return tipoat;
 		
 	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public List<JB_Tip_Ataque> selecConsu(String var){
+		Connection conn = null;
+		PreparedStatement state = null;
+		ResultSet result = null;
+		JB_Tip_Ataque Tat = null;
+		
+			List<JB_Tip_Ataque> tipoat = new ArrayList<>();
+			
+			try {
+				
+				conn = Conexion.getConnection();
+				state = conn.prepareStatement(selectConsu);
+				
+				state.setString(1, var);
+				
+				result = state.executeQuery();
+				
+					while(result.next()) {
+						
+						String id_at = result.getString("id_at");
+						String nom_tipo = result.getString("nom_tipo");
+						String reg_afect = result.getString("reg_afect");
+						String mutant_afect = result.getString("mutant_afect");
+						String grupo_at = result.getString("grupo_at");
+						String alien = result.getString("alien");
+						String bienes = result.getString("bienes");
+						
+						Tat = new JB_Tip_Ataque(id_at,nom_tipo,reg_afect,mutant_afect,grupo_at,alien,bienes);
+						tipoat.add(Tat);
+						
+					}
+					
+					Conexion.close(result);
+					Conexion.close(state);
+					Conexion.close(conn);
+					
+					for(JB_Tip_Ataque c: tipoat) {
+						System.out.println("id_at: " + c.getIdAt());
+						System.out.println("Nombre tipo: " + c.getNomTipo());
+						System.out.println("reg afect: " + c.getRegAfect());
+						System.out.println("mutant afect: " + c.getMutantAfect());
+						System.out.println("grupo at: " + c.getGrupoAt());
+						System.out.println("alien: " + c.getAlien());
+						System.out.println("bienes: " + c.getBienes());
+					}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return tipoat;
+		
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
 	
 	public int agregar(JB_Tip_Ataque Tipo) {
 		
