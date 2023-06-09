2979d597d00453bb24db758c501537fe70ddf29f
Darkexx <fertojonathan@gmail.com>
2023-06-09 00:36:16 -0600

    agregada una consulta para mostrar los nombres de las subdivisiones

--- src/main/java/Datos/DAO_Lideres_Subdv.java
+++ src/main/java/Datos/DAO_Lideres_Subdv.java
@@ -10,6 +10,7 @@ public class DAO_Lideres_Subdv {
 	public static final String insertSQL = "INSERT INTO lideres_subdivision(id_ls,nombre,rango,id_sub) VALUES (?,?,?,?)";
 	public static final String updateSQL = "UPDATE lideres_subdivision SET nombre = ?, rango = ?, id_sub = ? WHERE id_ls = ?";
 	public static final String deleteSQL = "DELETE FROM lideres_subdivision WHERE id_ls=?";
+	public static final String consuLid = "SELECT lideres_subdivision.id_ls,lideres_subdivision.nombre,lideres_subdivision.rango,subdivision.nombre as Subdivision  FROM lideres_subdivision join subdivision on lideres_subdivision.id_sub=subdivision.id_sub";
 	
 	public List<JB_Lideres_Subdivision> seleccionar(){
 		Connection conn = null;
@@ -55,6 +56,56 @@ public class DAO_Lideres_Subdv {
 			return lidsub;
 		
 	}
+	
+	
+	////////////////////////////////////////////////////////////////////////////////////////
+	
+	public List<JB_Lideres_Subdivision> consuLid(){
+		Connection conn = null;
+		Statement state = null;
+		ResultSet result = null;
+		JB_Lideres_Subdivision Lis = null;
+		
+			List<JB_Lideres_Subdivision> lidsub = new ArrayList<>();
+			
+			try {
+				
+				conn = Conexion.getConnection();
+				state = conn.createStatement();
+				result = state.executeQuery(consuLid);
+				
+					while(result.next()) {
+						
+						String id_ls = result.getString("id_ls");
+						String nombre = result.getString("nombre");
+						String rango = result.getString("rango");
+						String Subdivision = result.getString("Subdivision");
+						
+						Lis = new JB_Lideres_Subdivision(id_ls,nombre,rango,Subdivision);
+						lidsub.add(Lis);
+						
+					}
+					
+					Conexion.close(result);
+					Conexion.close(state);
+					Conexion.close(conn);
+					
+					for(JB_Lideres_Subdivision c: lidsub) {
+						System.out.println("id_ls: " + c.getIdLs());
+						System.out.println("Nombre: " + c.getNombre());
+						System.out.println("Rango: " + c.getRango());
+						System.out.println("id_sub: " + c.getIdSub());
+					}
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return lidsub;
+		
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////
 	
 	public int agregar(JB_Lideres_Subdivision Lider) {
 		
