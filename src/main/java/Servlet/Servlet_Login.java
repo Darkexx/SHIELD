84ac5931754940a705da436e36c55e62a441791a
Darkexx <fertojonathan@gmail.com>
2023-06-09 17:33:51 -0600

    Servlet mejorado para el index de integrante

--- src/main/java/Servlet/Servlet_Login.java
+++ src/main/java/Servlet/Servlet_Login.java
@@ -50,16 +50,8 @@ public class Servlet_Login extends HttpServlet{
                 }
                 
                 else if (usur.getRoll().equals("Integrante")) {
-                    //Si se trata de un Lider_Heroes obtenemos su nombre 
-                	
-                	int idint = Integer.parseInt(usur.getId_us());
-                	
-                    DAO_Lider lidao = new DAO_Lider();
-                    String nomlid = lidao.retnom(idint);
 
-                    request.setAttribute("nomlider",nomlid);
-
-                    RequestDispatcher dispatcher = request.getRequestDispatcher("");
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("indexIntegrante.jsp");
                     dispatcher.forward(request, response);
                 }
                 
